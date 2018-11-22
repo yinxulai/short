42bde457b17cde9f7bde259542ab9dd9ffaf2edc
阿来 <yinxulai@hotmail.com>
2018-11-22 21:48:56 +0800

    init

---
+++ lib/short.dart
@@ -0,0 +1,5 @@
+library short;
+
+export './short_tab_bar.dart';
+export './short_tab_bar_view.dart';
+export './short_tob_controller.dart';
---
+++ lib/short_tab_bar_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class ShortTabBarView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("ShortTabBarView");
+  }
+}
